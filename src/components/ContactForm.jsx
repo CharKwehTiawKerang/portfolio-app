fb3c4f9485cfd6eb8b983f042ddb274acc04aee2
Hariz Asyraf <takamagahara16@gmail.com>
2024-05-12 22:00:35 +0800

    ✏️ Fix some env variables and working on contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,43 +1,110 @@
-import React, { useState } from 'react'
-import { Form, Input, Typography } from 'antd'
+import React, { useState, useRef } from 'react'
+import { Form, Input } from 'antd'
 import emailjs from '@emailjs/browser'
 
 const ContactForm = () => {
+    const form = useRef();
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [stateMessage, setStateMessage] = useState(null);
 
     const sendEmail = (e) => {
-        // e.persist();
-        e.preventDefault();
+        console.log('MASUK!');
         setIsSubmitting(true);
 
         emailjs.sendForm(
-            process.env.REACT_APP_SERVICE_ID,
-            process.env.REACT_APP_TEMPLATE_ID,
-            e.target,
-            process.env.REACT_APP_PUBLIC_KEY
-        ).then(
-            (result) => {
-                setStateMessage('Message sent!');
-                setIsSubmitting(false);
-                setTimeout(() =>  {
-                    setStateMessage(null);
-                }, 5000); // hide message after 5 seconds
-            }, (error) => {
-                setStateMessage('Something went wrong, please try again later');
-                setIsSubmitting(false);
-                setTimeout(() =>  {
-                    setStateMessage(null);
-                }, 5000); // hide message after 5 seconds
-            }
-        );
+            import.meta.env.VITE_SERVICE_ID,
+            import.meta.env.VITE_TEMPLATE_ID,
+            form.current,
+            { 
+                publicKey: import.meta.env.VITE_PUBLIC_KEY 
+            }).then(
+                () => {
+                  console.log('SUCCESS!');
+                },
+                (error) => {
+                  console.log('FAILED...', error.text);
+                },
+              );
 
-        e.target.reset();
     }
 
   return (
-    <Form onSubmit={sendEmail}>
-        
+    <Form className='contact-form' ref={form} onFinish={sendEmail} layout='vertical' autoComplete='off'>
+        <Form.Item
+            label="NAME"
+            name="name"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please fill in your name.',
+                },
+            ]}
+        >
+            <Input placeholder='Your Name'/>
+        </Form.Item>
+
+        <Form.Item
+            label="CONTACT NUMBER"
+            name="phone"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please fill in your contact number.',
+                },
+            ]}
+        >
+            <Input placeholder='+60' type='number'/>
+        </Form.Item>
+
+        <Form.Item
+            label="EMAIL"
+            name="email"
+            rules={[
+              {
+                type: 'email',
+                required: true,
+                message: 'The input is not valid email format.',
+              },
+            ]}
+        >
+            <Input placeholder='name@company.com'/>
+        </Form.Item>
+
+        <Form.Item
+            label="SUBJECT"
+            name="subject"
+            rules={[
+              {
+                required: true,
+                message: "Can't submit an empty topic.",
+              },
+            ]}
+        >
+            <Input placeholder='Title'/>
+        </Form.Item>
+
+        <Form.Item
+            label="MESSAGE"
+            name="message"
+            rules={[
+              {
+                required: true,
+                message: "Can't submit an empty message.",
+              },
+            ]}
+        >
+            <Input.TextArea showCount maxLength={1000} placeholder='Maximum 1000 letters'/>
+        </Form.Item>
+
+        <Form.Item
+            wrapperCol={{
+                offset: 8,
+                span: 16,
+            }}
+        >
+            <button className='submit-contact' disabled={isSubmitting}>SUBMIT</button>
+            { stateMessage && <p className='error-submit'>{stateMessage}</p> }
+        </Form.Item>
     </Form>
   )
 }
